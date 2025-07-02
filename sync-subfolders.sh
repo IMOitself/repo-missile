05a5046588092f811d7f64db3791efdf4a73b9e5
IMOitself <russjonn@gmail.com>
2025-07-02 21:15:10 +0800

    refactor: use built-in git commands

--- sync-subfolders.sh
+++ sync-subfolders.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -e
 
-# 1. Create a temporary directory for the filtered history
-FILTERED_REPO_DIR=$(mktemp -d)
+# 1. Create a temporary directory for the source repo
+SOURCE_REPO_DIR=$(mktemp -d)
 
-# 2. Clone the source repo and filter it
+# 2. Clone the source repo
 echo "Cloning $SOURCE_REPO..."
-git clone --no-local --single-branch "https://github.com/$SOURCE_REPO.git" "$FILTERED_REPO_DIR"
-git -C "$FILTERED_REPO_DIR" filter-repo --path "$SUBFOLDER_PATH" --path-rename "$SUBFOLDER_PATH:"
+git clone --no-local --single-branch "https://github.com/$SOURCE_REPO.git" "$SOURCE_REPO_DIR"
 
-# 3. Sync the subfolder (no need to clone dest repo, we are in it)
-# Add the filtered repo as a remote
-git remote add filtered "$FILTERED_REPO_DIR"
-git fetch filtered
+# 3. In the source repo, create a branch with only the subfolder history
+echo "Creating branch with subfolder history..."
+git -C "$SOURCE_REPO_DIR" subtree split --prefix "$SUBFOLDER_PATH" -b filtered-branch
 
-# Use git subtree to merge the histories
+# We are running inside the destination repo checkout.
+# 4. Add the local source repo clone as a remote
+git remote add source_clone "$SOURCE_REPO_DIR"
+git fetch source_clone
+
+# 5. Use git subtree to add the subfolder from the filtered branch
 echo "Syncing subfolder with commit history..."
-git subtree add --prefix "$SUBFOLDER_PATH" filtered/master
+git subtree add --prefix "$SUBFOLDER_PATH" source_clone/filtered-branch
+
+# 6. Clean up the remote
+git remote remove source_clone
 
 echo "Subfolder sync complete. Changes are ready to be committed."
