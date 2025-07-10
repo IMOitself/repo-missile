042d8e707be38146340629fc65f1e421f81c5300
IMOitself <IMOitself@users.noreply.github.com>
2025-07-10 17:41:44 +0800

    test: port main.sh to action.sh

--- action.sh
+++ action.sh
@@ -26,6 +26,8 @@ rm -rf .git
 
 A="A"
 B="B"
+A_SUBFOLDER="$SOURCE_FILES"
+B_SUBFOLDER="$DESTINATION_DIRECTORY"
 
 echo "Cloning source and target repositories..."
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_REPOSITORY.git" $A
@@ -41,21 +43,22 @@ sync_tag="#repo-missile"
 initialize_sync_on_repo_if_needed(){
     repo_folder="$1"
     repo_name="$2"
+    subfolder_to_sync="$3"
 
     (
     cd "$repo_folder" || exit 1
 
-    last_sync_commit_hash=$(git log --grep="$sync_tag" -n 1 2>/dev/null)
+    last_sync_commit_hash=$(git log --grep="$sync_tag" -n 1 2>/dev/null) # not really a commit hash
 
     if [ -z "$last_sync_commit_hash" ]; then
-        m="repo-missile: initial setup"
         git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
         git config user.name "github-actions[bot]"
-        git commit --allow-empty -m "$m" -m "$sync_tag made in $repo_name" >/dev/null 2>&1
+        git commit --allow-empty -m "repo-missile: initial setup" -m "$sync_tag made in $repo_name" >/dev/null 2>&1
 
         #reset user config
         git config user.email "$default_user_email"
         git config user.name "$default_user_name"
+        git push
 
         echo 1
     else
@@ -68,15 +71,16 @@ push_action(){
     source_repo_folder="$1"
     target_repo_folder="$2"
     source_repo_name="$3"
-    target_repo_name="$4"
+    source_subfolder_to_sync="$4"
+    target_subfolder_to_sync="$5"
     
     echo ""
     echo "source: $source_repo_folder"
     echo "target: $target_repo_folder"
 
     is_initial=0
-    is_initial=$(initialize_sync_on_repo_if_needed "$source_repo_folder" "$source_repo_name")
-    is_initial=$(initialize_sync_on_repo_if_needed "$target_repo_folder" "$source_repo_name")
+    is_initial=$(initialize_sync_on_repo_if_needed "$source_repo_folder" "$source_repo_name" "$source_subfolder_to_sync")
+    is_initial=$(initialize_sync_on_repo_if_needed "$target_repo_folder" "$source_repo_name" "$target_subfolder_to_sync")
 
     if [[ "$is_initial" -eq 1 ]]; then
         echo INITIAL SETUP COMPLETE
@@ -85,21 +89,33 @@ push_action(){
 
     is_source_at_last_sync=0
     is_target_at_last_sync=0
-    sync_tag="#repo-missile"
 
     is_source_at_last_sync=$( \
     repo_folder=$source_repo_folder
     cd "$repo_folder" || exit
-    # check if the last commit has the sync tag. does not need to be subfolder when scope is implemented
-    echo $(git log -1 --pretty=format:%B | grep -q "$sync_tag" && echo 1 || echo 0)
+
+    result=$(git log -1 --pretty=format:%B -- "$source_subfolder_to_sync" 2>/dev/null | grep -q "$sync_tag" && echo 1 || echo 0)
+
+    if [[ -z "$result" || "$result" -eq 0 ]]; then
+        # check if the last commit that has a sync tag is not in the subfolder
+        result=$(git log -1 --pretty=format:%B 2>/dev/null | grep -q "$sync_tag" && echo 1 || echo 0)
+    fi
+    echo $result
     )
 
     is_target_at_last_sync=$( \
     repo_folder=$target_repo_folder
     cd "$repo_folder" || exit
-    # check if the last commit has the sync tag. does not need to be subfolder when scope is implemented
-    echo $(git log -1 --pretty=format:%B | grep -q "$sync_tag" && echo 1 || echo 0)
+    
+    result=$(git log -1 --pretty=format:%B -- "$target_subfolder_to_sync" 2>/dev/null | grep -q "$sync_tag" && echo 1 || echo 0)
+    
+    if [[ -z "$result" || "$result" -eq 0 ]]; then
+        # check if the last commit that has a sync tag is not in the subfolder
+        result=$(git log -1 --pretty=format:%B 2>/dev/null | grep -q "$sync_tag" && echo 1 || echo 0)
+    fi
+    echo $result
     )
+
 
     if [[ "$is_source_at_last_sync" -eq 1 && "$is_target_at_last_sync" -eq 1 ]]; then
         echo "BOTH REPOS ALREADY IN SYNC"
@@ -111,47 +127,62 @@ push_action(){
         echo "giving commits to $target_repo_folder..."
         
         (
-        cd "$source_repo_folder" || exit
-        source_last_sync_commit_hash=$(git log --grep="$sync_tag" -n 1 --pretty=format:%H)
-        mapfile -t commit_list < <(git log --reverse --oneline --pretty=format:%H "$source_last_sync_commit_hash"..HEAD)
+    cd "$source_repo_folder" || exit
+    source_last_sync_commit_hash=$(git log --grep="$sync_tag" -n 1 --pretty=format:%H)
+    mapfile -t commit_list < <(git log --reverse --pretty=format:%H "$source_last_sync_commit_hash"..HEAD -- "$source_subfolder_to_sync")
+
+    cd ..
+    cd "$target_repo_folder" || exit
+
+    for commit_hash in "${commit_list[@]}"; do
+        echo "Applying commit: $commit_hash"
+
+        COMMIT_MSG=$(git --git-dir="../$source_repo_folder/.git" log --format=%B -n 1 "$commit_hash")
+        COMMIT_AUTHOR=$(git --git-dir="../$source_repo_folder/.git" log --format="%an <%ae>" -n 1 "$commit_hash")
+        COMMIT_DATE=$(git --git-dir="../$source_repo_folder/.git" log --format=%ad -n 1 "$commit_hash")
+
+        git --git-dir="../$source_repo_folder/.git" diff "$commit_hash^..$commit_hash" -- "$source_subfolder_to_sync" | \
+            sed -e "s| a/$source_subfolder_to_sync| a/$target_subfolder_to_sync|g" \
+                -e "s| b/$source_subfolder_to_sync| b/$target_subfolder_to_sync|g" \
+            > /tmp/commit.patch
+        if git apply /tmp/commit.patch; then
+            git add "$target_subfolder_to_sync"
+            git commit --message="$COMMIT_MSG" --author="$COMMIT_AUTHOR" --date="$COMMIT_DATE"
+        else
+            echo "ERROR: Failed to apply patch for commit $commit_hash." >&2
+            echo "Manual intervention required. Patch file saved at /tmp/commit.patch" >&2
+            exit 1
+        fi
         
-        cd ..
-        cd "$target_repo_folder" || exit
-
-        git remote add temp_source "../$source_repo_folder"
-        git fetch temp_source
-
-        for commit_hash in "${commit_list[@]}"; do
-            echo "Applying commit: $commit_hash"
-        
-            if ! git cherry-pick "$commit_hash"; then
-                echo "ERROR: Cherry-pick failed for commit $commit_hash."
-                echo "Aborting the cherry-pick and the script."
-                git cherry-pick --abort
-                git remote remove temp_source # Clean up before exiting
-                exit 1
-            fi
-        done
-
-        git remote remove temp_source
-        )
+        rm /tmp/commit.patch
+    done
+)
         echo ""
         
     elif [[ "$is_source_at_last_sync" -eq 1 && "$is_target_at_last_sync" -eq 0 ]]; then
-        echo "TARGET REPO $target_repo_folder HAS MORE COMMITS"
-        echo "taking commits from $target_repo_folder..."
-        echo "TODO: implement"
         echo ""
+        echo "ERROR: TARGET REPO $target_repo_folder HAS MORE COMMITS"
+        echo "TODO: implement taking commits from $target_repo_folder"
+        echo ""
+        exit 1
         
+    elif [[ "$is_source_at_last_sync" -eq 0 && "$is_target_at_last_sync" -eq 0 ]]; then
+        echo ""
+        echo "ERROR: both repos have newer commits"
+        echo "TODO: implement taking commits from $target_repo_folder"
+        echo "TODO: then implement giving commits to $target_repo_folder"
+        echo ""
+        exit 1
     else
-        echo "BOTH REPOS HAVE NEWER COMMITS"
-        echo "taking commits from $target_repo_folder..."
-        echo "giving commits to $target_repo_folder..."
-        echo "TODO: implement"
+        echo ""
+        echo "ERROR: unknown state"
+        echo "is_source_at_last_sync: $is_source_at_last_sync"
+        echo "is_target_at_last_sync: $is_target_at_last_sync"
+        echo ""
+        exit 1
     fi
 
     echo "committing on both repos with $sync_tag for tracking..."
-    # amend the last commit of both repos to include the sync tag. does not need to be subfolder when scope is implemented
     (
     cd "$target_repo_folder" || exit
     git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
@@ -161,6 +192,7 @@ push_action(){
     #reset user config
     git config user.email "$default_user_email"
     git config user.name "$default_user_name"
+    git push
     
     cd ..
     cd "$source_repo_folder" || exit
@@ -169,7 +201,9 @@ push_action(){
     git commit --allow-empty -m "repo-missile: give commits to $target_repo_name" -m "$sync_tag"
     git config user.email "$default_user_email"
     git config user.name "$default_user_name"
+    git push
     )
 }
 
-push_action $A $B "IMOaswell/A" "IMOaswell/B"
+
+push_action $A $B "$GITHUB_REPOSITORY" "$A_SUBFOLDER" "$B_SUBFOLDER"
